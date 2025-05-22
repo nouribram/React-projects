8ee65e183d4b4f59c8e64ee71ecaf0268b191c90
nouribram <nourtesla@yahoo.com>
2025-05-22 11:40:46 +0300

    new update

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,51 +1,11 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-
-const hasLiked = true;
-
-
-const Card = ({ title }) => {
-    
-    const [count, setCount] = useState(0);
-    const [hasLiked, setHasLiked] = useState(false);
-
-  
-  useEffect(() => {
-  console.log(`${title} has been liked ${hasLiked}`);
-  }, [title, hasLiked]);
-
-   useEffect
-
-  return(
-
-     <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
-     
-      <h2>{title} <br/> {count}</h2>
-
-      <button onClick={() => setHasLiked(true)}>
-        {hasLiked ? '❤️' : '🤍'}
-      </button>
+export default function App() {
+  return (
+    <div>
+       <h1 class="text-3xl font-bold underline">
+        
+       </h1>
     </div>
   )
 }
-
-const App = () => {
-
-
-
-  return(
-
-    <div className='card-container'>
-
-      <Card title="Star Wars" rating={5} isCool={true} hasLiked={hasLiked}/>
-      <Card title="Avatar"/>
-      <Card title="Lion King"/>
-
-    </div>
-  )
-}
-
-export default App
