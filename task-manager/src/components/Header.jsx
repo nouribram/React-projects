6cf35666fa2f3eb8be580d7fdf9c267940afa6df
nouribram <nourtesla@yahoo.com>
2025-05-30 03:22:18 +0300

    new

---
+++ task-manager/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return(
+        <header>
+            <h1 className="text-gradient">you have 3 open tasks </h1>
+        </header>
+    )
+}
---
+++ task-manager/src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import { TodoCard } from "./TodoCard";
+
+export function TodoList() {
+    return (
+        <div>
+            <TodoCard />
+            <TodoCard />
+            <TodoCard />
+            <TodoCard />
+            <TodoCard />
+
+        </div>
+    )
+}
