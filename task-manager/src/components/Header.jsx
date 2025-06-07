5658471da15c82b08c602318d5e532d330ca47c5
nouribram <nourtesla@yahoo.com>
2025-06-07 18:26:27 +0300

    final

--- task-manager/src/components/Header.jsx
+++ task-manager/src/components/Header.jsx
@@ -1,14 +1,12 @@
 export function Header(props) {
-
     const { todos } = props
     const todosLength = todos.length
-    const isTaskPlural = todos.length != 1
-    
-    const taskOrTasks = isTaskPlural ? 'tasks' : 'task'
-    return(
+    const isTasksPlural = todos.length != 1
+    const taskOrTasks = isTasksPlural ? 'tasks' : 'task'
+
+    return (
         <header>
-            <h1 className="text-gradient">you have {todosLength}
-                open {taskOrTasks}</h1>
+            <h1 className="text-gradient">You have {todosLength} open {taskOrTasks}.</h1>
         </header>
     )
 }
--- task-manager/src/components/TodoList.jsx
+++ task-manager/src/components/TodoList.jsx
@@ -1,25 +1,29 @@
 import { TodoCard } from "./TodoCard";
 
 export function TodoList(props) {
-      
-    const { todos } = props
-    const tab = 'All'
-    const  filterTodoList = tab === 'All' ? 
-    todos : 
-    tab === 'Completed' ? 
-    todos.filter(val => val.completed):
-    todos.filter(val => !val.complete)
+    const { todos, selectedTab } = props
+
+
+    const filterTodosList = selectedTab === 'All' ?
+        todos :
+        selectedTab === 'Completed' ?
+            todos.filter(val => val.complete) :
+            todos.filter(val => !val.complete)
 
     return (
         <>
-         {filterTodoList.map((todo, todoIndex) => {
-          return (
-            <TodoCard
-             key={todoIndex}
-            todoIndex={todoIndex} 
-            todo={tood}/>
-          )
-       })}
+            {filterTodosList.map((todo, todoIndex) => {
+                const tempTodoIndex = todos.findIndex(val => val.input == todo.input)
+                console.log(tempTodoIndex)
+                // finding an index like this has it's limitations and in later courses we learn a more sophisticated way of doing it (the limitation is that the code might misbehave if you have two todos with the exact same text :) See if you can figure out why!)
+                return (
+                    <TodoCard
+                        key={todoIndex}
+                        {...props}
+                        todoIndex={tempTodoIndex}
+                        todo={todo} />
+                )
+            })}
 
         </>
     )
