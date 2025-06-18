1bfe59532104b6f265991791083e442c8f98ab9f
nouribram <nourtesla@yahoo.com>
2025-06-18 22:26:38 +0300

    .

--- poke/src/components/PokeCard.jsx
+++ poke/src/components/PokeCard.jsx
@@ -5,11 +5,14 @@ export function PokeCard(props) {
     const { selectedPokemon } = props
     const {data, setData} = useState(null)
     const {loading, setLoading} = useState(false)
+    const {name, height, abilities, stats, types, moves, sprites } = 
+    data || {}
 
     useEffect(() => {
        
         if (loading || !localStorage) { return }
         let cache = {} 
+
         if (localStorage.getItem('pokedex')) {
             cache = Json.parse(localStorage.getItem('pokemon'))
         }
@@ -22,7 +25,8 @@ export function PokeCard(props) {
             setLoading(true)
             try {
                const baseUrl = 'https://pokeapi.co/api/v2/'
-               const suffix = 'pokemon/' + selectedPokemon
+               const suffix = 'pokemon/' + getPokedexNumber
+               (selectedPokemon)
                const finalUrl = baseUrl + suffix
                const res = await fetch(finalUrl)
                const pokemonData = await res.json()
@@ -43,7 +47,28 @@ export function PokeCard(props) {
 
     }, [loading, selectedPokemon])
     
+    if (loading || !data) {
+
+        return (
+            <div>
+                <h4>Loading...</h4>
+            </div>
+        )
+    }
+
     return (
-        <div></div>
+        <div className="poke-card">
+            <div>
+                <h4>{getFullPokedexNumber(selectedPokemon)}</h4>
+                <h2>{name}</h2>
+            </div>
+            <div className="type-container">
+                {types.map((type, typeIndex) => {
+                    return (
+                        <TypeCard key={typeIndex} type={type}/>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
