2adbf49f34ff25d63e0fbae244df274a6e4722a2
nouribram <nourtesla@yahoo.com>
2025-07-11 17:23:37 +0300

    new

--- cs/src/components/Hero.jsx
+++ cs/src/components/Hero.jsx
@@ -2,7 +2,19 @@ export default function Hero() {
 
     return(
         <>
-             
+          <h1>Coffe Tracking for Coffe<abbr title="An enthusiast or devoted">Fiends</abbr></h1>
+           <div className="benefits-List">
+            <h3 className="font-bolder">Try <span className="text-gradient">caffined</span> and start...</h3>
+            <p>✅Tracking ever Coffe</p>
+            <p>✅Measuring your blood caffeine levels</p>
+            <p>✅Costing and quanitifying your addition</p>
+           </div>
+           <div className="card info-card">
+             <div>
+              <i className="fa-solid fa-circle-info"></i>
+              <h3>Did you know...</h3>
+             </div>
+           </div>
         </>
     )
 }
