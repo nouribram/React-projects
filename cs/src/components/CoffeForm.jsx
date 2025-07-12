81edca416116b79bcd6e2104485dac158fde3f8a
nouribram <nourtesla@yahoo.com>
2025-07-12 18:53:40 +0300

    new

--- cs/src/components/CoffeForm.jsx
+++ cs/src/components/CoffeForm.jsx
@@ -1,7 +1,33 @@
+import { coffeOptions } from "../utils"
+
+
+
+
+
 export default function CoffeForm() {
 
     return(
         <>
+
+         <div className="section-header">
+            <i className="fa-solid fa-pencil"/>
+            <h2>Start Tracking today</h2>
+         </div>
+         <h4>Select Coffet Type</h4>
+         <div className="coffe-grid">
+            {coffeOptions.slice(0, 5).map((option, optionIndex) => {
+                return (
+                    <button className="button-card" key={optionIndex}>
+                       <h4>{option.name}</h4>
+                       <p>{option.caffeine}mg</p>
+                    </button>
+                )
+            })}
+            <button className="button-card">
+                <h4>other</h4>
+                <p>n/a</p>
+            </button>
+         </div>
              
         </>
     )
